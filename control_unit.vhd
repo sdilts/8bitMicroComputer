f6b74e374246a9a41e6d9cbb0da74d4f618e42d8
sdilts <stuart.dilts@gmail.com>
2018-04-12 23:20:08 -0600

    Implemented loading and storing from register b
    + Also fixed error where PC was being incremented too many times

--- control_unit.vhd
+++ control_unit.vhd
@@ -23,6 +23,11 @@ architecture control_unit_arch of control_unit is
      S_LDA_IMM_4, S_LDA_IMM_5, S_LDA_IMM_6,
      S_LDA_DIR_4, S_LDA_DIR_5, S_LDA_DIR_6, S_LDA_DIR_7, S_LDA_DIR_8,
      S_STA_DIR_4, S_STA_DIR_5, S_STA_DIR_6, S_STA_DIR_7,
+
+     S_LDB_IMM_4, S_LDB_IMM_5, S_LDB_IMM_6,
+     S_LDB_DIR_4, S_LDB_DIR_5, S_LDB_DIR_6, S_LDB_DIR_7, S_LDB_DIR_8,
+     S_STB_DIR_4, S_STB_DIR_5, S_STB_DIR_6, S_STB_DIR_7,
+
      S_ADD_AB_4,
      S_BRA_4, S_BRA_5, S_BRA_6,
      S_BEQ_4, S_BEQ_5, S_BEQ_6, S_BEQ_7);
@@ -120,7 +125,6 @@ begin
         Bus1_Sel <= "00"; -- "00"=PC, "01"=A, "10"=B
         Bus2_Sel <= "10"; -- "00"=ALU, "01"=Bus1, "10"=from_memory
         w_bit <= '0';
-        -- hasn't been tested yet:
       when S_LDA_DIR_4 =>
         IR_Load <= '0';
         MAR_Load <= '1';
@@ -209,7 +213,7 @@ begin
         IR_Load <= '0';
         MAR_Load <= '1';
         PC_Load <= '0';
-        PC_Inc <= '1';
+        PC_Inc <= '0';
         A_Load <= '0';
         B_Load <= '0';
         CCR_Load <= '0';
@@ -221,13 +225,168 @@ begin
         IR_Load <= '0';
         MAR_Load <= '0';
         PC_Load <= '0';
-        PC_Inc <= '1';
+        PC_Inc <= '0';
         A_Load <= '0';
         B_Load <= '0';
         CCR_Load <= '0';
         w_bit <= '1';
         ALU_Sel <= "000";
         Bus1_Sel <= "01"; -- "00"=PC, "01"=A, "10"=B
+      when S_LDB_IMM_4 =>
+        IR_Load <= '0';
+        MAR_Load <= '1';
+        PC_Load <= '0';
+        PC_Inc <= '0';
+        A_Load <= '0';
+        B_Load <= '0';
+        ALU_Sel <= "000";
+        CCR_Load <= '0';
+        Bus1_Sel <= "00"; -- "00"=PC, "01"=A, "10"=B
+        Bus2_Sel <= "01"; -- "00"=ALU, "01"=Bus1, "10"=from_memory
+        w_bit <= '0';
+      when S_LDB_IMM_5 =>
+        IR_Load <= '0';
+        MAR_Load <= '0';
+        PC_Load <= '0';
+        PC_Inc <= '1';
+        A_Load <= '0';
+        B_Load <= '0';
+        ALU_Sel <= "000";
+        CCR_Load <= '0';
+        Bus1_Sel <= "00"; -- "00"=PC, "01"=A, "10"=B
+        Bus2_Sel <= "01"; -- "00"=ALU, "01"=Bus1, "10"=from_memory
+        w_bit <= '0';
+      when S_LDB_IMM_6 =>
+        IR_Load <= '0';
+        MAR_Load <= '0';
+        PC_Load <= '0';
+        PC_Inc <= '0';
+        A_Load <= '0';
+        B_Load <= '1';
+        ALU_Sel <= "000";
+        CCR_Load <= '0';
+        Bus1_Sel <= "00"; -- "00"=PC, "01"=A, "10"=B
+        Bus2_Sel <= "10"; -- "00"=ALU, "01"=Bus1, "10"=from_memory
+        w_bit <= '0';
+      when S_LDB_DIR_4 =>
+        IR_Load <= '0';
+        MAR_Load <= '1';
+        PC_Load <= '0';
+        PC_Inc <= '0';
+        A_Load <= '1';
+        B_Load <= '0';
+        ALU_Sel <= "000";
+        CCR_Load <= '0';
+        Bus1_Sel <= "00"; -- "00"=PC, "01"=A, "10"=B
+        Bus2_Sel <= "01"; -- "00"=ALU, "01"=Bus1, "10"=from_memory
+        w_bit <= '0';
+      when S_LDB_DIR_5 =>
+        IR_Load <= '0';
+        MAR_Load <= '0';
+        PC_Load <= '0';
+        PC_Inc <= '1';
+        A_Load <= '0';
+        B_Load <= '0';
+        ALU_Sel <= "000";
+        CCR_Load <= '0';
+        Bus1_Sel <= "00"; -- "00"=PC, "01"=A, "10"=B
+        Bus2_Sel <= "01"; -- "00"=ALU, "01"=Bus1, "10"=from_memory
+        w_bit <= '0';
+      when S_LDB_DIR_6 =>
+        IR_Load <= '0';
+        MAR_Load <= '1';
+        PC_Load <= '0';
+        PC_Inc <= '0';
+        A_Load <= '0';
+        B_Load <= '0';
+        ALU_Sel <= "000";
+        CCR_Load <= '0';
+        Bus1_Sel <= "00"; -- "00"=PC, "01"=A, "10"=B
+        Bus2_Sel <= "10"; -- "00"=ALU, "01"=Bus1, "10"=from_memory
+        w_bit <= '0';
+      when S_LDB_DIR_7 =>
+        IR_Load <= '0';
+        MAR_Load <= '0';
+        PC_Load <= '0';
+        PC_Inc <= '0';
+        A_Load <= '0';
+        B_Load <= '0';
+        ALU_Sel <= "000";
+        CCR_Load <= '0';
+        Bus1_Sel <= "00"; -- "00"=PC, "01"=A, "10"=B
+        Bus2_Sel <= "10"; -- "00"=ALU, "01"=Bus1, "10"=from_memory
+        w_bit <= '0';
+      when S_LDB_DIR_8 =>
+        IR_Load <= '0';
+        MAR_Load <= '1';
+        PC_Load <= '0';
+        PC_Inc <= '0';
+        A_Load <= '0';
+        B_Load <= '1';
+        ALU_Sel <= "000";
+        CCR_Load <= '0';
+        Bus1_Sel <= "00"; -- "00"=PC, "01"=A, "10"=B
+        Bus2_Sel <= "10"; -- "00"=ALU, "01"=Bus1, "10"=from_memory
+        w_bit <= '0';
+      when S_STB_DIR_4 =>
+        IR_Load <= '0';
+        MAR_Load <= '1';
+        PC_Load <= '0';
+        PC_Inc <= '0';
+        A_Load <= '0';
+        B_Load <= '0';
+        ALU_Sel <= "000";
+        CCR_Load <= '0';
+        Bus1_Sel <= "00"; -- "00"=PC, "01"=A, "10"=B
+        Bus2_Sel <= "01"; -- "00"=ALU, "01"=Bus1, "10"=from_memory
+        w_bit <= '0';
+      when S_STB_DIR_5 =>
+        IR_Load <= '0';
+        MAR_Load <= '0';
+        PC_Load <= '0';
+        PC_Inc <= '1';
+        A_Load <= '0';
+        B_Load <= '0';
+        CCR_Load <= '0';
+        w_bit <= '0';
+        ALU_Sel <= "000";
+        Bus1_Sel <= "00"; -- "00"=PC, "01"=A, "10"=B
+        Bus2_Sel <= "01"; -- "00"=ALU, "01"=Bus1, "10"=from_memory
+      when S_STB_DIR_6 =>
+        IR_Load <= '0';
+        MAR_Load <= '1';
+        PC_Load <= '0';
+        PC_Inc <= '0';
+        A_Load <= '0';
+        B_Load <= '0';
+        CCR_Load <= '0';
+        w_bit <= '0';
+        ALU_Sel <= "000";
+        Bus1_Sel <= "00"; -- "00"=PC, "01"=A, "10"=B
+        Bus2_Sel <= "10"; -- "00"=ALU, "01"=Bus1, "10"=from_memory
+      when S_STB_DIR_7 =>
+        IR_Load <= '0';
+        MAR_Load <= '0';
+        PC_Load <= '0';
+        PC_Inc <= '0';
+        A_Load <= '0';
+        B_Load <= '0';
+        CCR_Load <= '0';
+        w_bit <= '1';
+        ALU_Sel <= "000";
+        Bus1_Sel <= "10"; -- "00"=PC, "01"=A, "10"=B
+      when S_ADD_AB_4 =>
+        IR_Load <= '0';
+        MAR_Load <= '0';
+        PC_Load <= '0';
+        PC_Inc <= '0';
+        A_Load <= '1';
+        B_Load <= '0';
+        ALU_Sel <= "000";
+        CCR_Load <= '1';
+        Bus1_Sel <= "01"; -- "00"=PC, "01"=A, "10"=B
+        Bus2_Sel <= "00"; -- "00"=ALU, "01"=Bus1, "10"=from_memory
+        w_bit <= '0';
       when others =>
         report "Shouldn't get here" severity failure;
     end case;
@@ -252,6 +411,7 @@ begin
       next_state <= S_DECODE_3;
     elsif (current_state = S_DECODE_3) then
       -- select execution path
+      -- A LOADS and STORES ---------------------------------
       if (IR = LDA_IMM) then
         -- Load A Immediate
         next_state <= S_LDA_IMM_4;
@@ -261,6 +421,17 @@ begin
       elsif (IR = STA_DIR) then
         -- Store A Direct
         next_state <= S_STA_DIR_4;
+      -- B LOADS and STORES --------------------------------
+      elsif (IR = LDB_IMM) then
+        -- Load B Immediate
+        next_state <= S_LDB_IMM_4;
+      elsif (IR = LDB_DIR) then
+        -- Load B Direct
+        next_state <= S_LDB_DIR_4;
+      elsif (IR = STB_DIR) then
+        -- Store B Direct
+        next_state <= S_STB_DIR_4;
+      -- ALU instructions -----------------------------------
       elsif (IR = ADD_AB) then
         -- Add A and B
         next_state <= S_ADD_AB_4;
@@ -276,14 +447,14 @@ begin
       else
         next_state <= S_FETCH_0;
       end if;
-      -- Load immediate states ---------------------------------------
+      -- LDA_IMM states ---------------------------------------
     elsif current_state = S_LDA_IMM_4 then
       next_state <= S_LDA_IMM_5;
     elsif current_state = S_LDA_IMM_5 then
       next_state <= S_LDA_IMM_6;
     elsif current_state = S_LDA_IMM_6 then
       next_state <= S_FETCH_0;
-      -- LDA_DIR states ------------------------------------------------
+    -- LDA_DIR states ------------------------------------------------
     elsif current_state = S_LDA_DIR_4 then
       next_state <= S_LDA_DIR_5;
     elsif current_state = S_LDA_DIR_5 then
@@ -302,6 +473,36 @@ begin
     elsif current_state = S_STA_DIR_6 then
       next_state <= S_STA_DIR_7;
     elsif current_state = S_STA_DIR_7 then
+      next_state <= S_FETCH_0;
+    -- LDB_IMM states ---------------------------------------
+    elsif current_state = S_LDB_IMM_4 then
+      next_state <= S_LDB_IMM_5;
+    elsif current_state = S_LDB_IMM_5 then
+      next_state <= S_LDB_IMM_6;
+    elsif current_state = S_LDB_IMM_6 then
+      next_state <= S_FETCH_0;
+    -- LDB_DIR states ------------------------------------------------
+    elsif current_state = S_LDB_DIR_4 then
+      next_state <= S_LDB_DIR_5;
+    elsif current_state = S_LDB_DIR_5 then
+      next_state <= S_LDB_DIR_6;
+    elsif current_state = S_LDB_DIR_6 then
+      next_state <= S_LDB_DIR_7;
+    elsif current_state = S_LDB_DIR_7 then
+      next_state <= S_LDB_DIR_8;
+    elsif current_state = S_LDB_DIR_8 then
+      next_state <= S_FETCH_0;
+    -- STB_DIR states --------------------------------------------
+    elsif current_state = S_STB_DIR_4 then
+      next_state <= S_STB_DIR_5;
+    elsif current_state = S_STB_DIR_5 then
+      next_state <= S_STB_DIR_6;
+    elsif current_state = S_STB_DIR_6 then
+      next_state <= S_STB_DIR_7;
+    elsif current_state = S_STB_DIR_7 then
+      next_state <= S_FETCH_0;
+    -- ADD_AB states:
+    elsif current_state = S_ADD_AB_4 then
       next_state <= S_FETCH_0;
     else
       report "Shouldn't get to else in if statement" severity failure;
