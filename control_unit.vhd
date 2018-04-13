175b64b173f942ba6debb03fc90c24d871f768b3
sdilts <stuart.dilts@gmail.com>
2018-04-12 22:34:36 -0600

    Implemented STA_DIR

--- control_unit.vhd
+++ control_unit.vhd
@@ -22,7 +22,7 @@ architecture control_unit_arch of control_unit is
      S_DECODE_3,
      S_LDA_IMM_4, S_LDA_IMM_5, S_LDA_IMM_6,
      S_LDA_DIR_4, S_LDA_DIR_5, S_LDA_DIR_6, S_LDA_DIR_7, S_LDA_DIR_8,
-     S_STA_DIR_4, S_STA_DIR_5, S_STA_DIR_6, S_STA_DIR_7, S_STA_DIR_8,
+     S_STA_DIR_4, S_STA_DIR_5, S_STA_DIR_6, S_STA_DIR_7,
      S_ADD_AB_4,
      S_BRA_4, S_BRA_5, S_BRA_6,
      S_BEQ_4, S_BEQ_5, S_BEQ_6, S_BEQ_7);
@@ -181,6 +181,53 @@ begin
         Bus1_Sel <= "00"; -- "00"=PC, "01"=A, "10"=B
         Bus2_Sel <= "10"; -- "00"=ALU, "01"=Bus1, "10"=from_memory
         w_bit <= '0';
+      when S_STA_DIR_4 =>
+        IR_Load <= '0';
+        MAR_Load <= '1';
+        PC_Load <= '0';
+        PC_Inc <= '0';
+        A_Load <= '0';
+        B_Load <= '0';
+        ALU_Sel <= "000";
+        CCR_Load <= '0';
+        Bus1_Sel <= "00"; -- "00"=PC, "01"=A, "10"=B
+        Bus2_Sel <= "01"; -- "00"=ALU, "01"=Bus1, "10"=from_memory
+        w_bit <= '0';
+      when S_STA_DIR_5 =>
+        IR_Load <= '0';
+        MAR_Load <= '0';
+        PC_Load <= '0';
+        PC_Inc <= '1';
+        A_Load <= '0';
+        B_Load <= '0';
+        CCR_Load <= '0';
+        w_bit <= '0';
+        ALU_Sel <= "000";
+        Bus1_Sel <= "00"; -- "00"=PC, "01"=A, "10"=B
+        Bus2_Sel <= "01"; -- "00"=ALU, "01"=Bus1, "10"=from_memory
+      when S_STA_DIR_6 =>
+        IR_Load <= '0';
+        MAR_Load <= '1';
+        PC_Load <= '0';
+        PC_Inc <= '1';
+        A_Load <= '0';
+        B_Load <= '0';
+        CCR_Load <= '0';
+        w_bit <= '0';
+        ALU_Sel <= "000";
+        Bus1_Sel <= "00"; -- "00"=PC, "01"=A, "10"=B
+        Bus2_Sel <= "10"; -- "00"=ALU, "01"=Bus1, "10"=from_memory
+      when S_STA_DIR_7 =>
+        IR_Load <= '0';
+        MAR_Load <= '0';
+        PC_Load <= '0';
+        PC_Inc <= '1';
+        A_Load <= '0';
+        B_Load <= '0';
+        CCR_Load <= '0';
+        w_bit <= '1';
+        ALU_Sel <= "000";
+        Bus1_Sel <= "01"; -- "00"=PC, "01"=A, "10"=B
       when others =>
         report "Shouldn't get here" severity failure;
     end case;
@@ -246,6 +293,15 @@ begin
     elsif current_state = S_LDA_DIR_7 then
       next_state <= S_LDA_DIR_8;
     elsif current_state = S_LDA_DIR_8 then
+      next_state <= S_FETCH_0;
+      -- STA_DIR states --------------------------------------------
+    elsif current_state = S_STA_DIR_4 then
+      next_state <= S_STA_DIR_5;
+    elsif current_state = S_STA_DIR_5 then
+      next_state <= S_STA_DIR_6;
+    elsif current_state = S_STA_DIR_6 then
+      next_state <= S_STA_DIR_7;
+    elsif current_state = S_STA_DIR_7 then
       next_state <= S_FETCH_0;
     else
       report "Shouldn't get to else in if statement" severity failure;
