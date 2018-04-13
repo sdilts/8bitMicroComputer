6d091aca701719c1100e3fa006a229d3bdfc5e31
sdilts <stuart.dilts@gmail.com>
2018-04-12 19:25:29 -0600

    Implemented ALU

--- alu.vhd
+++ alu.vhd
@@ -18,4 +18,59 @@ architecture alu_arc of alu is
 
 begin
 
+
+  ALU_PROCESS : process(A, B, ALU_Sel)
+    variable sum_uns : unsigned(8 downto 0);
+  begin
+    if ALU_Sel = "000" then
+      -- addition
+      sum_uns := unsigned('0' & A) + unsigned('0' & B);
+      Result <= std_logic_vector(sum_uns(word_width downto 0));
+      -- Negative flag
+      NZVC(3) <= sum_uns(7);
+
+      --zero flag (Z)
+      if sum_uns(word_width downto 0) = x"00" then
+        NZVC(2) <= '1';
+      else
+        NZVC(2) <= '0';
+      end if;
+
+      --overflow flag
+      if ((A(7) = '0' and B(7) = '0' and sum_uns(7) = '1') or
+          (A(7) = '1' and B(7) = '1' and sum_uns(7) = '0')) then
+        NZVC(1) <= '1';
+      else
+        NZVC(1) <= '0';
+      end if;
+
+      -- carry flag
+      NZVC(0) <= sum_uns(8);
+    elsif ALU_Sel = "001" then
+      -- addition
+      sum_uns := unsigned('0' & A) - unsigned('0' & B);
+      Result <= std_logic_vector(sum_uns);
+      -- Negative flag
+      NZVC(3) <= sum_uns(7);
+
+      --zero flag (Z)
+      if sum_uns(word_width downto 0) = x"00" then
+        NZVC(2) <= '1';
+      else
+        NZVC(2) <= '0';
+      end if;
+
+      --overflow flag
+      if ((A(7) = '0' and B(7) = '0' and sum_uns(7) = '1') or
+          (A(7) = '1' and B(7) = '1' and sum_uns(7) = '0')) then
+        NZVC(1) <= '1';
+      else
+        NZVC(1) <= '0';
+      end if;
+      -- carry flag
+      NZVC(0) <= sum_uns(8);
+    else
+      report "Invalid ALU_Sel";
+    end if;
+  end process;
 end architecture;
--- data_path.vhd
+++ data_path.vhd
@@ -5,12 +5,13 @@ use IEEE.NUMERIC_STD.all;
 use WORK.cpu_constants.all;
 
 entity data_path is
-  port(IR_Load, MAR_Load, PC_Load, PC_inc, A_Load, B_Load, CCR_Load : in std_logic;
+  port(clock, reset : in std_logic;
+       IR_Load, MAR_Load, PC_Load, PC_inc, A_Load, B_Load, CCR_Load : in std_logic;
        Bus2_Sel, Bus1_Sel : in std_logic_vector(1 downto 0);
        ALU_Sel : in std_logic_vector(2 downto 0);
        from_memory : in std_logic_vector(word_width downto 0);
        CCR_Result : out std_logic_vector(3 downto 0);
-       IR  : out std_logic;
+       IR  : out std_logic_vector(word_width downto 0);
        address, to_memory : out std_logic_vector(word_width downto 0));
 end entity;
 
@@ -32,21 +33,20 @@ architecture data_path_arch of data_path is
   end component;
 
   signal bus1, bus2 : std_logic_vector(word_width downto 0);
-  signal ALU_Sel : std_logic_vector(2 downto 0);
   -- TODO: This thing needs a real name:
-  signal ALU_a_in, ALU_b_in  : std_logic_vector(word_width downto 0);
   signal ALU_result : std_logic_vector(word_width downto 0);
-  signal CCR_in     : std_logic_vector(3 downto 0);
-  -- TODO: Clarify what A and B are:
-  signal PC, A, B   : std_logic_vector(word_width downto 0);
+  signal NZVC       : std_logic_vector(3 downto 0);
+  -- The registers:
+  signal PC, A, B, MAR   : std_logic_vector(word_width downto 0);
+  signal PC_uns : unsigned(word_width downto 0);
 
 begin
 
   ARITM_UNIT : alu port map(ALU_Sel => ALU_Sel,
                      -- TODO: give this ALU_a_in, ALU_b_in real input names. See above
-                     A       => ALU_a_in,
-                     B       => ALU_b_in,
-                     NZVC    => CCR_in,
+                     A       => A,
+                     B       => B,
+                     NZVC    => NZVC,
                      Result  => ALU_result);
 
   BUS1_multi : multiplexer_3t1 port map(one => PC,
