17b107841ba17ef00cf389aa5722988e1706a850
sdilts <stuart.dilts@gmail.com>
2018-04-16 15:28:02 -0600

    Fixed some bugs, converted if.. else to case statement

--- control_unit.vhd
+++ control_unit.vhd
@@ -496,6 +496,18 @@ begin
         Bus1_Sel <= "00"; -- "00"=PC, "01"=A, "10"=B
         Bus2_Sel <= "00"; -- "00"=ALU, "01"=Bus1, "10"=from_memory
         w_bit <= '0';
+      when S_BRANCH_4 =>
+        IR_Load <= '0';
+        MAR_Load <= '1';
+        PC_Load <= '0';
+        PC_Inc <= '0';
+        A_Load <= '0';
+        B_Load <= '0';
+        ALU_Sel <= "000";
+        CCR_Load <= '0';
+        Bus1_Sel <= "00"; -- "00"=PC, "01"=A, "10"=B
+        Bus2_Sel <= "01"; -- "00"=ALU, "01"=Bus1, "10"=from_memory
+        w_bit <= '0';
       when S_BRANCH_5 =>
         -- do nothing:
         IR_Load <= '0';
@@ -576,8 +588,8 @@ begin
         MAR_Load <= '0';
         PC_Load <= '0';
         PC_Inc <= '0';
-        A_Load <= '1';
-        B_Load <= '0';
+        A_Load <= '0';
+        B_Load <= '1';
         ALU_Sel <= ALU_INCB;
         CCR_Load <= '1';
         Bus1_Sel <= "01"; -- "00"=PC, "01"=A, "10"=B
@@ -600,15 +612,15 @@ begin
         MAR_Load <= '0';
         PC_Load <= '0';
         PC_Inc <= '0';
-        A_Load <= '1';
-        B_Load <= '0';
+        A_Load <= '0';
+        B_Load <= '1';
         ALU_Sel <= ALU_DECB;
         CCR_Load <= '1';
         Bus1_Sel <= "01"; -- "00"=PC, "01"=A, "10"=B
         Bus2_Sel <= "00"; -- "00"=ALU, "01"=Bus1, "10"=from_memory
         w_bit <= '0';
-      when others =>
-        report "Shouldn't get here" severity failure;
+      -- when others =>
+      --   report "Shouldn't get here" severity failure;
     end case;
   end process;
 
@@ -623,187 +635,194 @@ begin
 
   NEXT_STATE_LOGIC : process(current_state, IR, CCR_Result)
   begin
-    -- fetch states ----------------------------------------
-    if (current_state = S_FETCH_0) then
-      next_state <= S_FETCH_1;
-    elsif (current_state = S_FETCH_1) then
-      next_state <= S_FETCH_2;
-    elsif (current_state = S_FETCH_2) then
-      next_state <= S_DECODE_3;
-    elsif (current_state = S_DECODE_3) then
-      -- select execution path
-      -- A LOADS and STORES ---------------------------------
-      if (IR = LDA_IMM) then
-        -- Load A Immediate
-        next_state <= S_LDA_IMM_4;
-      elsif (IR = LDA_DIR) then
-        -- Load A Direct
-        next_state <= S_LDA_DIR_4;
-      elsif (IR = STA_DIR) then
-        -- Store A Direct
-        next_state <= S_STA_DIR_4;
-      -- B LOADS and STORES --------------------------------
-      elsif (IR = LDB_IMM) then
-        -- Load B Immediate
-        next_state <= S_LDB_IMM_4;
-      elsif (IR = LDB_DIR) then
-        -- Load B Direct
-        next_state <= S_LDB_DIR_4;
-      elsif (IR = STB_DIR) then
-        -- Store B Direct
-        next_state <= S_STB_DIR_4;
-      -- ALU instructions -----------------------------------
-      elsif (IR = ADD_AB) then
-        -- Add A and B
-        next_state <= S_ADD_AB_4;
-      elsif (IR = SUB_AB) then
-        -- Add A and B
-        next_state <= S_SUB_AB_4;
-      elsif IR = OR_AB then
-        next_state <= S_OR_AB;
-      elsif IR = AND_AB then
-        next_state <= S_AND_AB;
-      elsif IR = INCA then
-        next_state <= S_INCA;
-      elsif IR = DECA then
-        next_state <= S_DECA;
-      elsif IR =INCB then
-        next_state <= S_INCB;
-      elsif IR = DECB then
-        next_state <= S_DECB;
-      -- Branching instructions -----------------------------
-      elsif (IR = BRA) then
-        -- Branch Always
-        next_state <= S_BRA_4;
-      elsif IR = BCS then
-        if CCR_Result(3) = '1' then
-          next_state <= S_BRANCH_4;
-        else
-          next_state <= S_BRANCH_7;
-        end if;
-      elsif (IR=BEQ and CCR_Result(2)='1') then
-        -- BEQ and Z=1
-        next_state <= S_BEQ_4;
-      elsif (IR=BEQ and CCR_Result(2)='0') then
-        -- BEQ and Z=0
-        next_state <= S_BEQ_7;
-      elsif IR = BVS then
-        if CCR_Result(1) = '1' then
-          next_state <= S_BRANCH_4;
-        else
-          next_state <= S_BRANCH_7;
-        end if;
-      elsif IR = BMI then
-        if CCR_Result(0) = '1' then
-          next_state <= S_BRANCH_4;
-        else
-          next_state <= S_BRANCH_7;
+    case current_state is
+      -- fetch states ----------------------------------------
+      when S_FETCH_0 =>
+        next_state <= S_FETCH_1;
+      when S_FETCH_1 =>
+        next_state <= S_FETCH_2;
+      when S_FETCH_2 =>
+        next_state <= S_DECODE_3;
+      when S_DECODE_3 =>
+        -- select execution path
+        -- A LOADS and STORES ---------------------------------
+        if (IR = LDA_IMM) then
+          -- Load A Immediate
+          next_state <= S_LDA_IMM_4;
+        elsif (IR = LDA_DIR) then
+          -- Load A Direct
+          next_state <= S_LDA_DIR_4;
+        elsif (IR = STA_DIR) then
+          -- Store A Direct
+          next_state <= S_STA_DIR_4;
+        -- B LOADS and STORES --------------------------------
+        elsif (IR = LDB_IMM) then
+          -- Load B Immediate
+          next_state <= S_LDB_IMM_4;
+        elsif (IR = LDB_DIR) then
+          -- Load B Direct
+          next_state <= S_LDB_DIR_4;
+        elsif (IR = STB_DIR) then
+          -- Store B Direct
+          next_state <= S_STB_DIR_4;
+        -- ALU instructions -----------------------------------
+        elsif (IR = ADD_AB) then
+          -- Add A and B
+          next_state <= S_ADD_AB_4;
+        elsif (IR = SUB_AB) then
+          -- Add A and B
+          next_state <= S_SUB_AB_4;
+        elsif IR = OR_AB then
+          next_state <= S_OR_AB;
+        elsif IR = AND_AB then
+          next_state <= S_AND_AB;
+        elsif IR = INCA then
+          next_state <= S_INCA;
+        elsif IR = DECA then
+          next_state <= S_DECA;
+        elsif IR =INCB then
+          next_state <= S_INCB;
+        elsif IR = DECB then
+          next_state <= S_DECB;
+        -- Branching instructions -----------------------------
+        elsif (IR = BRA) then
+          -- Branch Always
+          next_state <= S_BRA_4;
+        elsif IR = BCS then
+          if CCR_Result(3) = '1' then
+            next_state <= S_BRANCH_4;
+          else
+            next_state <= S_BRANCH_7;
+          end if;
+        elsif (IR=BEQ and CCR_Result(2)='1') then
+          -- BEQ and Z=1
+          next_state <= S_BEQ_4;
+        elsif (IR=BEQ and CCR_Result(2)='0') then
+          -- BEQ and Z=0
+          next_state <= S_BEQ_7;
+        elsif IR = BVS then
+          if CCR_Result(1) = '1' then
+            next_state <= S_BRANCH_4;
+          else
+            next_state <= S_BRANCH_7;
+          end if;
+        elsif IR = BMI then
+          if CCR_Result(0) = '1' then
+            next_state <= S_BRANCH_4;
+          else
+            next_state <= S_BRANCH_7;
+          end if;
         end if;
       -- ALU States -------------------------------------
-      elsif current_state = S_OR_AB then
+      when S_OR_AB =>
         next_state <= S_FETCH_0;
-      elsif current_state = S_AND_AB then
+      when S_AND_AB =>
         next_state <= S_FETCH_0;
-      elsif current_state = S_INCA then
+      when S_INCA =>
         next_state <= S_FETCH_0;
-      elsif current_state = S_DECA then
+      when S_DECA =>
         next_state <= S_FETCH_0;
-      elsif current_state = S_INCB then
+      when S_INCB =>
         next_state <= S_FETCH_0;
-      elsif current_state = S_DECB then
+      when S_DECB =>
         next_state <= S_FETCH_0;
-      else
+      -- Branching states ------------------------------------
+      when S_BRANCH_4 =>
+        next_state <= S_BRANCH_5;
+      when S_BRANCH_5 =>
+        next_state <= S_BRANCH_6;
+      when S_BRANCH_6 =>
         next_state <= S_FETCH_0;
-      end if;
-    -- Branching states ------------------------------------
-    elsif current_state = S_BRANCH_4 then
-      next_state <= S_BRANCH_5;
-    elsif current_state = S_BRANCH_5 then
-      next_state <= S_BRANCH_6;
-    elsif current_state = S_BRANCH_6 then
-      next_state <= S_FETCH_0;
-    elsif current_state = S_BRANCH_7 then
-      next_state <= S_FETCH_0;
-    -- LDA_IMM states ---------------------------------------
-    elsif current_state = S_LDA_IMM_4 then
-      next_state <= S_LDA_IMM_5;
-    elsif current_state = S_LDA_IMM_5 then
-      next_state <= S_LDA_IMM_6;
-    elsif current_state = S_LDA_IMM_6 then
-      next_state <= S_FETCH_0;
-    -- LDA_DIR states ------------------------------------------------
-    elsif current_state = S_LDA_DIR_4 then
-      next_state <= S_LDA_DIR_5;
-    elsif current_state = S_LDA_DIR_5 then
-      next_state <= S_LDA_DIR_6;
-    elsif current_state = S_LDA_DIR_6 then
-      next_state <= S_LDA_DIR_7;
-    elsif current_state = S_LDA_DIR_7 then
-      next_state <= S_LDA_DIR_8;
-    elsif current_state = S_LDA_DIR_8 then
-      next_state <= S_FETCH_0;
-    -- STA_DIR states --------------------------------------------
-    elsif current_state = S_STA_DIR_4 then
-      next_state <= S_STA_DIR_5;
-    elsif current_state = S_STA_DIR_5 then
-      next_state <= S_STA_DIR_6;
-    elsif current_state = S_STA_DIR_6 then
-      next_state <= S_STA_DIR_7;
-    elsif current_state = S_STA_DIR_7 then
-      next_state <= S_FETCH_0;
-    -- LDB_IMM states ---------------------------------------
-    elsif current_state = S_LDB_IMM_4 then
-      next_state <= S_LDB_IMM_5;
-    elsif current_state = S_LDB_IMM_5 then
-      next_state <= S_LDB_IMM_6;
-    elsif current_state = S_LDB_IMM_6 then
-      next_state <= S_FETCH_0;
-    -- LDB_DIR states ------------------------------------------------
-    elsif current_state = S_LDB_DIR_4 then
-      next_state <= S_LDB_DIR_5;
-    elsif current_state = S_LDB_DIR_5 then
-      next_state <= S_LDB_DIR_6;
-    elsif current_state = S_LDB_DIR_6 then
-      next_state <= S_LDB_DIR_7;
-    elsif current_state = S_LDB_DIR_7 then
-      next_state <= S_LDB_DIR_8;
-    elsif current_state = S_LDB_DIR_8 then
-      next_state <= S_FETCH_0;
-    -- STB_DIR states --------------------------------------------
-    elsif current_state = S_STB_DIR_4 then
-      next_state <= S_STB_DIR_5;
-    elsif current_state = S_STB_DIR_5 then
-      next_state <= S_STB_DIR_6;
-    elsif current_state = S_STB_DIR_6 then
-      next_state <= S_STB_DIR_7;
-    elsif current_state = S_STB_DIR_7 then
-      next_state <= S_FETCH_0;
-    -- ADD_AB states: ---------------------------------------
-    elsif current_state = S_ADD_AB_4 then
-      next_state <= S_FETCH_0;
-    -- SUB_AB states:----------------------------------------
-    elsif current_state = S_SUB_AB_4 then
-      next_state <= S_FETCH_0;
-    -- BRA instruction
-    elsif current_state = S_BRA_4 then
-      next_state <= S_BRA_5;
-    elsif current_state = S_BRA_5 then
-      next_state <= S_BRA_6;
-    elsif current_state = S_BRA_6 then
-      next_state <= S_FETCH_0;
-    -- BEQ instruction
-    elsif current_state = S_BEQ_4 then
-      next_state <= S_BEQ_5;
-    elsif current_state = S_BEQ_5 then
-      next_state <= S_BEQ_6;
-    elsif current_state = S_BEQ_6 then
-      next_state <= S_FETCH_0;
-    elsif current_state = S_BEQ_7 then
-      next_state <= S_FETCH_0;
-    else
-      report "Shouldn't get to else in if statement" severity failure;
-    end if;
-  end process;
+      when S_BRANCH_7 =>
+        next_state <= S_FETCH_0;
+      -- LDA_IMM states ---------------------------------------
+      when S_LDA_IMM_4 =>
+        next_state <= S_LDA_IMM_5;
+      when S_LDA_IMM_5 =>
+        next_state <= S_LDA_IMM_6;
+      when S_LDA_IMM_6 =>
+        next_state <= S_FETCH_0;
+      -- LDA_DIR states ------------------------------------------------
+      when S_LDA_DIR_4 =>
+        next_state <= S_LDA_DIR_5;
+      when S_LDA_DIR_5 =>
+        next_state <= S_LDA_DIR_6;
+      when S_LDA_DIR_6 =>
+        next_state <= S_LDA_DIR_7;
+      when S_LDA_DIR_7 =>
+        next_state <= S_LDA_DIR_8;
+      when S_LDA_DIR_8 =>
+        next_state <= S_FETCH_0;
+      -- STA_DIR states --------------------------------------------
+      when S_STA_DIR_4 =>
+        next_state <= S_STA_DIR_5;
+      when S_STA_DIR_5 =>
+        next_state <= S_STA_DIR_6;
+      when S_STA_DIR_6 =>
+        next_state <= S_STA_DIR_7;
+      when S_STA_DIR_7 =>
+        next_state <= S_FETCH_0;
+      -- LDB_IMM states ---------------------------------------
+      when S_LDB_IMM_4 =>
+        next_state <= S_LDB_IMM_5;
+      when S_LDB_IMM_5 =>
+        next_state <= S_LDB_IMM_6;
+      when S_LDB_IMM_6 =>
+        next_state <= S_FETCH_0;
+      -- LDB_DIR states ------------------------------------------------
+      when S_LDB_DIR_4 =>
+        next_state <= S_LDB_DIR_5;
+      when S_LDB_DIR_5 =>
+        next_state <= S_LDB_DIR_6;
+      when S_LDB_DIR_6 =>
+        next_state <= S_LDB_DIR_7;
+      when S_LDB_DIR_7 =>
+        next_state <= S_LDB_DIR_8;
+      when S_LDB_DIR_8 =>
+        next_state <= S_FETCH_0;
+      -- STB_DIR states --------------------------------------------
+      when S_STB_DIR_4 =>
+        next_state <= S_STB_DIR_5;
+      when S_STB_DIR_5 =>
+        next_state <= S_STB_DIR_6;
+      when S_STB_DIR_6 =>
+        next_state <= S_STB_DIR_7;
+      when S_STB_DIR_7 =>
+        next_state <= S_FETCH_0;
+      -- ALU states: ---------------------------------------
+      when S_SUB_AB_4 =>
+        next_state <= S_FETCH_0;
+      when S_ADD_AB_4 =>
+        next_state <= S_FETCH_0;
+      -- when S_DECA =>
+      --   next_state <= S_FETCH_0;
+      -- when S_INCA  =>
+      --   next_state <= S_FETCH_0;
+      -- when S_OR_AB =>
+      --   next_state <= S_FETCH_0;
+      -- when S_AND_AB =>
+      --   next_state <= S_FETCH_0;
+      -- BRA instruction
+      when S_BRA_4 =>
+        next_state <= S_BRA_5;
+      when S_BRA_5 =>
+        next_state <= S_BRA_6;
+      when S_BRA_6 =>
+        next_state <= S_FETCH_0;
+      -- BEQ instruction
+      when S_BEQ_4 =>
+        next_state <= S_BEQ_5;
+      when S_BEQ_5 =>
+        next_state <= S_BEQ_6;
+      when S_BEQ_6 =>
+        next_state <= S_FETCH_0;
+      when S_BEQ_7 =>
+        next_state <= S_FETCH_0;
+      -- when others =>
+      --   report "Value is: " & state_type'image(current_state);
+      --   report "Shouldn't get to else in if statement" severity failure;
+  end case;
+end process;
 
 
 end architecture;
